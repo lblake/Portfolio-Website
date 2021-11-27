e8efd15fe519c4cee8ffb541183ad19445532e44
lblake <treleven.lloyd@gmail.com>
2021-11-27 18:47:51 +0000

    formatted file

--- portfolio-website/src/components/IntroComponent/intro.js
+++ portfolio-website/src/components/IntroComponent/intro.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import styles  from "./intro.module.css"
+import React from 'react';
+import styles from './intro.module.css';
 
 function Intro() {
-    return (
-        <div className={styles.intro}>
-            <div className={styles.introRight}>Right
-            <div className={styles.introLeftWrapper}>
-                <h2 className={styles.introIntro}>Hello, My name is</h2>
-                <h1 className={styles.introName}>Lloyd Blake</h1>
-            </div>
-            </div>
-            <div className={styles.introLeft}>Left</div>
+  return (
+    <div className={styles.intro}>
+      <div className={styles.introRight}>
+        Right
+        <div className={styles.introLeftWrapper}>
+          <h2 className={styles.introIntro}>Hello, My name is</h2>
+          <h1 className={styles.introName}>Lloyd Blake</h1>
         </div>
-    )
+      </div>
+      <div className={styles.introLeft}>Left</div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
