31c60544f50bec351c73132cdae486d649241ae8
lblake <treleven.lloyd@gmail.com>
2021-11-27 21:47:16 +0000

    updated intro component

--- portfolio-website/src/components/IntroComponent/intro.js
+++ portfolio-website/src/components/IntroComponent/intro.js
@@ -5,10 +5,20 @@ function Intro() {
   return (
     <div className={styles.intro}>
       <div className={styles.introRight}>
-        Right
         <div className={styles.introLeftWrapper}>
           <h2 className={styles.introIntro}>Hello, My name is</h2>
           <h1 className={styles.introName}>Lloyd Blake</h1>
+          <div className={styles.introTitle}>
+            <div className={styles.introTitleWrapper}>
+              <div className={styles.introTitleItem}>Web Developer</div>
+              <div className={styles.introTitleItem}>
+                Senior Automation Engineer
+              </div>
+              <div className={styles.introTitleItem}>Senior QA Engineer</div>
+              <div className={styles.introTitleItem}>PPC/SEO Excutive</div>
+              <div className={styles.introTitleItem}>Test Analyst</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.introLeft}>Left</div>
