fad24d1032e74bc48ce81fd95a4eb389911a1fca
lblake <treleven.lloyd@gmail.com>
2021-12-01 22:06:10 +0000

    updated about component

--- portfolio-website/src/components/AboutComponent/About.js
+++ portfolio-website/src/components/AboutComponent/About.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import styles from './about.module.css'
+import React from 'react';
+import styles from './about.module.css';
 
 export default function About() {
   return (
     <div className={styles.about}>
-      
+      <div className={styles.aboutLeft}>
+        <div className={styles.aboutCardBackground}></div>
+        <div className={styles.aboutCard}>
+          <img
+            src='https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+            alt=''
+            className={styles.aboutImage}
+          />
+        </div>
+      </div>
+
+      <div className={styles.aboutRight}></div>
     </div>
-  )
+  );
 }
