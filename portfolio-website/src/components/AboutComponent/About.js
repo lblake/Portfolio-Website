e5b251b441193a520dadc561a9095f82fdf04150
lblake <treleven.lloyd@gmail.com>
2021-12-05 11:55:04 +0000

    updated about component

--- portfolio-website/src/components/AboutComponent/About.js
+++ portfolio-website/src/components/AboutComponent/About.js
@@ -8,14 +8,27 @@ export default function About() {
         <div className={styles.aboutCardBackground}></div>
         <div className={styles.aboutCard}>
           <img
-            src='https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
-            alt=''
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
             className={styles.aboutImage}
           />
         </div>
       </div>
-
-      <div className={styles.aboutRight}></div>
+      <div className={styles.aboutLeft}>
+        <h1 className={styles.aboutTitle}>About Me</h1>
+        <p className={styles.SubHeading}>
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className={styles.aboutDescription}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+      
+      </div>
     </div>
-  );
+      
+    );
 }
