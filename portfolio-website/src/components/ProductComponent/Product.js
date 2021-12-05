18c03c0b8e27f3eee2337d69b9b0f5a7d747af5a
lblake <treleven.lloyd@gmail.com>
2021-12-05 21:37:25 +0000

    updated product components

--- portfolio-website/src/components/ProductComponent/Product.js
+++ portfolio-website/src/components/ProductComponent/Product.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import styles from './product.module.css';
 
-export default function Product() {
-  return <div className={styles.product}></div>;
+export default function Product({ link, img }) {
+  return (
+    <div className={styles.product}>
+      <div className={styles.productBrowser}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+      </div>
+      <a href={link} target='_blank' rel='noreferrer'>
+        <img src={img} alt='' className='p-img' />
+      </a>
+    </div>
+  );
 }
--- portfolio-website/src/components/ProductListComponent/ProductList.js
+++ portfolio-website/src/components/ProductListComponent/ProductList.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import styles from './productList.module.css'
+import React from 'react';
+import styles from './productList.module.css';
+import Product from '../ProductComponent/Product';
+import { products } from '../../data';
 
 export default function ProductList() {
-    return (
-        <div className={styles.productList}>
-            
-        </div>
-    )
+  return (
+    <div className={styles.productList}>
+      <div className={styles.productListText}>
+        <h1 className={styles.productListTitle}>Create & inspire. It's Lama</h1>
+        <p className={styles.productListDescription}>
+          Lama is a creative portfolio that your work has been waiting for.
+          Beautiful homes, stunning portfolio styles & a whole lot more awaits
+          inside.
+        </p>
+      </div>
+      <div className={styles.productList}>
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
 }
