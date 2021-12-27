13fc50ce798a88482385ba8945adc004dcfbf156
lblake <treleven.lloyd@gmail.com>
2021-12-27 12:52:55 +0000

    updated css to use overlay effect

--- portfolio-website/src/components/ProductComponent/Product.js
+++ portfolio-website/src/components/ProductComponent/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './product.module.css';
 
-export default function Product({ link, img, title }) {
+export default function Product({ link, img }) {
   return (
     <div className={styles.product}>
       <div className={styles.productBrowser}>
@@ -10,10 +10,14 @@ export default function Product({ link, img, title }) {
         <div className={styles.circle}></div>
         {/* <div className={styles.productTitle}></div> */}
       </div>
-
-      <a href={link} target='_blank' rel='noopener noreferrer'>
-        <img src={img} alt='' className={styles.productImage} />
-      </a>
+      <div className={styles.container}>
+        <a href={link} target='_blank' rel='noopener noreferrer'>
+          <img src={img} alt='' className={styles.productImage} />
+        </a>
+        <div className={styles.overLay}>
+          <div className={styles.text}>Hello World</div>
+        </div>
+      </div>
     </div>
   );
 }
