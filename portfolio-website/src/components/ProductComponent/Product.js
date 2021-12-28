aa86f4306515f4311271a1a92198f70222be7751
lblake <treleven.lloyd@gmail.com>
2021-12-28 00:16:07 +0000

    removed some text and updated prop name

--- portfolio-website/src/components/ProductComponent/Product.js
+++ portfolio-website/src/components/ProductComponent/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './product.module.css';
 
-export default function Product({ link, img, title }) {
+export default function Product({ link, img, description }) {
   return (
     <div className={styles.product}>
       <div className={styles.productBrowser}>
@@ -15,9 +15,7 @@ export default function Product({ link, img, title }) {
           <img src={img} alt='' className={styles.productImage} />
         </a>
         <div className={styles.overLay}>
-          <div className={styles.text}>{title}
-          
-           </div>
+          <div className={styles.text}>{description}</div>
         </div>
       </div>
     </div>
--- portfolio-website/src/components/ProductListComponent/ProductList.js
+++ portfolio-website/src/components/ProductListComponent/ProductList.js
@@ -9,7 +9,7 @@ export default function ProductList() {
       <div className={styles.productListText}>
         <h1 className={styles.productListTitle}>Create & inspire. {/*It's Lama*/}</h1>
         <p className={styles.productListDescription}>
-          Below are some of the projects that I have completed.
+          Below are some of the projects that I created.
           {/* Beautiful homes, stunning portfolio styles & a whole lot more awaits
           inside. */}
         </p>
@@ -20,7 +20,7 @@ export default function ProductList() {
             key={item.id}
             img={item.img}
             link={item.link}
-            title={item.description}
+            description={item.description}
           />
         ))}
       </div>
