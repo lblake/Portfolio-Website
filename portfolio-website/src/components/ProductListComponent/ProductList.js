b6e1f353993c94f8131721ed070a27d31e8c5109
lblake <treleven.lloyd@gmail.com>
2022-01-05 18:36:52 +0000

    updated css for media quiries

--- portfolio-website/src/components/ProductListComponent/ProductList.js
+++ portfolio-website/src/components/ProductListComponent/ProductList.js
@@ -7,7 +7,7 @@ export default function ProductList() {
   return (
     <div className={styles.product}>
       <div className={styles.productListText}>
-        <h1 className={styles.productListTitle}>Create & inspire. {/*It's Lama*/}</h1>
+        <h1 className={styles.productListTitle}>Create & Inspire {/*It's Lama*/}</h1>
         <p className={styles.productListDescription}>
           Below are some of the projects that I created.
           {/* Beautiful homes, stunning portfolio styles & a whole lot more awaits
