13b2af5c91b6a74fdbb13897268082d239057cfa
lblake <treleven.lloyd@gmail.com>
2022-01-16 16:36:08 +0000

    refactored contact section

--- portfolio-website/src/components/ContactComponent/Contact.js
+++ portfolio-website/src/components/ContactComponent/Contact.js
@@ -34,70 +34,12 @@ export default function Contact() {
   };
 
   return (
-    <div className={styles.contact}>
-      <div className={styles.contactBackground}></div>
-      <div className={styles.contactWrapper}></div>
-      <div className={styles.contactLeft}>
-        <h1 className={styles.contactTitle}>Contact Me</h1>
-        <div className={styles.contactInfo}>
-          {/* <div className={styles.contactInfoItem}>
-            <FontAwesomeIcon
-              icon={faPhoneAlt}
-              className={styles.contactIcon}
-            ></FontAwesomeIcon>
-            +44 7800-634-799
-          </div> */}
+    <div className={styles.contactContainer}>
+      <h1 className={styles.contactTitle}>Contact Me</h1>
+      <a href='mailto:treleven.lloyd@gmail.com'>
+      <button className={styles.contactButton}>email me</button>
 
-          {/* <div className={styles.contactInfo}>
-            <FontAwesomeIcon
-              icon={faEnvelopeSquare}
-              className={styles.contactIcon}
-            ></FontAwesomeIcon>
-            treleven.lloyd@gmail.com
-          </div> */}
-          {/* <div className={styles.contactInfoItem}>
-            <FontAwesomeIcon
-              icon={faMapMarked}
-              className={styles.contactIcon}
-            ></FontAwesomeIcon>
-            Purley, Surrey
-          </div> */}
-        </div>
-      </div>
-      <div className={styles.contactRight}>
-        <p className={styles.contactDescription}>
-          <b>What’s your story?</b> Get in touch. Always available for
-          freelancing if the right project comes along. me.
-        </p>
-        <form ref={formRef} onSubmit={handleSubmit}>
-          <input
-            style={{ backgroundColor: '#3d79c9' }}
-            type='text'
-            placeholder='Name'
-            name='user_name'
-          />
-          <input
-            style={{ backgroundColor: '#3d79c9' }}
-            type='text'
-            placeholder='Subject'
-            name='user_subject'
-          />
-          <input
-            style={{ backgroundColor: '#3d79c9' }}
-            type='text'
-            placeholder='Email'
-            name='user_email'
-          />
-          <textarea
-            style={{ backgroundColor: '#3d79c9' }}
-            rows='5'
-            placeholder='Message'
-            name='message'
-          />
-          <button>Submit</button>
-          {done && 'Thank you...'}
-        </form>
-      </div>
+      </a>
     </div>
   );
 }
