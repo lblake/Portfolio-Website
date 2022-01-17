ef4201ec70ab1a5d7c22be8d4236bb149a2183e3
lblake <treleven.lloyd@gmail.com>
2022-01-17 20:02:16 +0000

    created new skills section

---
+++ portfolio-website/src/components/SkillsComponent/skillsData.js
@@ -0,0 +1,32 @@
+export const icons = [
+  {
+    id: 1,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg',
+    alt: 'html5',
+  },
+  {
+    id: 2,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg',
+    alt: 'css3',
+  },
+  {
+    id: 3,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    alt: 'javascript',
+  },
+  {
+    id: 4,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
+    alt: 'react',
+  },
+  {
+    id: 5,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg',
+    alt: 'git',
+  },
+  {
+    id: 6,
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    alt: 'jest',
+  },
+];
