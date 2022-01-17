19bb0da66b097ffcc0409075426f8905093995a2
lblake <treleven.lloyd@gmail.com>
2022-01-17 22:42:48 +0000

    removed unwamted code and fixed issue with map function

--- portfolio-website/src/components/SkillsComponent/Skills.js
+++ portfolio-website/src/components/SkillsComponent/Skills.js
@@ -6,51 +6,12 @@ export default function skills() {
   return (
     <div className={styles.skillsContainer}>
       <h1 className={styles.skillsTitle}>Skills</h1>
-      {/* icons are displayed virtically even though using same css */}
-      {icons.map((icon) => (
-          <ul className={styles.skillsList}>
-              <li className={styles.skillsList} key={icon.id}>  <img src={icon.img} alt={icon.alt}/> </li>
-          </ul>
-        ))}
-
-{/* icons displayed correctly using the same css */}
       <ul className={styles.skillsList}>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg'
-            alt=''
-          />
-        </li>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg'
-            alt=''
-          />
-        </li>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-            alt=''
-          />
-        </li>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'
-            alt=''
-          />
-        </li>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg'
-            alt=''
-          />
-        </li>
-        <li className={styles.skillsList}>
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg'
-            alt=''
-          />
-        </li>
+        {icons.map((icon) => (
+          <li className={styles.skillsList} key={icon.id}>
+            <img src={icon.img} alt={icon.alt} />
+          </li>
+        ))}
       </ul>
     </div>
   );
