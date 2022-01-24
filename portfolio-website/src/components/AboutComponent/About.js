bfa5e3a8a051d90a57b0e1b867cc7ad21a50cbec
lblake <treleven.lloyd@gmail.com>
2022-01-24 11:04:23 +0000

    updated css for mobile first design

--- portfolio-website/src/components/AboutComponent/About.js
+++ portfolio-website/src/components/AboutComponent/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './about.module.css';
-import image from './Lloyd-Desk.jpg'
+import image from './Lloyd-Desk.jpg';
 
 export default function About() {
   return (
@@ -8,19 +8,12 @@ export default function About() {
       <div className={styles.aboutLeft}>
         <div className={styles.aboutCardBackground}></div>
         <div className={styles.aboutCard}>
-          <img
-            src={image}
-            alt=''
-            className={styles.aboutImage}
-          />
+          <img src={image} alt='' className={styles.aboutImage} />
         </div>
       </div>
       <div className={styles.aboutRight}>
         <h1 className={styles.aboutTitle}>About Me</h1>
-        {/* <p className={styles.SubHeading}>
-          It is a long established fact that a reader will be distracted by the
-          readable content.
-        </p> */}
+
         <p className={styles.aboutDescription}>
           I'm Frontend developer who loves to convert beautiful designs, into
           pixel perfect web interfaces. I have a really good eye for details, so
