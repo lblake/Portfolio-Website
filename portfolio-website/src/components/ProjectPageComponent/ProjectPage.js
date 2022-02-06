cdebb7de6f83f062a266a93b1808de02994d6256
lblake <treleven.lloyd@gmail.com>
2022-02-06 18:08:40 +0000

    updated css to include transparent text over images

--- portfolio-website/src/components/ProjectPageComponent/ProjectPage.js
+++ portfolio-website/src/components/ProjectPageComponent/ProjectPage.js
@@ -2,25 +2,45 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { projects } from './projectData';
 import styles from './project.module.css';
-import Footer from '../FooterComponent/Footer'
-
+import Footer from '../FooterComponent/Footer';
 
 export default function ProjectPage() {
   const { projectName } = useParams();
 
   return (
     <div className={styles.container}>
-    <div >
-      {/* <img className={styles.mainImage} src={projects[projectName].topPricingImage} alt=''/> */}
-    </div>
-    <h1 className={styles.heading}>{projects[projectName].title}</h1>
-      {/* <p>{projects[projectName].description}</p> */}
-      <img 
+      <img
         className={styles.backgroundImage}
         src={projects[projectName].image}
         alt='projects'
       />
-      <Footer/>
+
+      <div className={styles.content}>
+        <h1 className={styles.heading}>{projects[projectName].title}</h1>
+        <p className={styles.description}>
+          {projects[projectName].description}
+        </p>
+        <div className={styles.center}>
+          <a
+            className={styles.button}
+            href={projects[projectName].link}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Website
+          </a>
+          <a
+            className={styles.button}
+            href={projects[projectName].github}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Github
+          </a>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
--- portfolio-website/src/components/ProjectPageComponent/projectData.js
+++ portfolio-website/src/components/ProjectPageComponent/projectData.js
@@ -2,26 +2,41 @@ import pricingImage from './images/Pricing.jpg';
 import weatherImage from './images/Weather.jpeg';
 import columnImage from './images/Blocks.jpg';
 import filterImage from './images/Filter.jpg';
-import topPricingImage from './images/pricing-component-annually-screenshot.png'
 
 export const projects = {
   pricing: {
     title: 'CSS, HTML, JavaScript Component with Toggle',
-    description: '',
-    topPricingImage:topPricingImage,
+    description:
+      'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
     image: pricingImage,
+    link: 'https://frontend-mentor-pricing-component-with-toggle.netlify.app/',
+    github:
+      'https://github.com/lblake/Front-End-Projects/tree/main/pricing-component-with-toggle',
   },
   weather: {
-    description: 'React API Weather App',
+    title: 'React Weather API',
+    description:
+      'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
     image: weatherImage,
-  },
-  users: {
-    description: 'React API Users App',
-    image:filterImage,
-  },
-  column:{
-    description: 'CSS, HTML 3 Column Preview Component',
-    image:columnImage
-  }
-};
+    link: 'https://api-react-weather.netlify.app/',
+    github: 'https://github.com/lblake/react-weather-api',
 
+    users: {
+      title: 'React API Users App',
+      description:
+        'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
+      image: filterImage,
+      link: 'https://react-users-api.netlify.app/',
+      github: 'https://github.com/lblake/react-api-users',
+    },
+  },
+  column: {
+    title: '3-Column Preview Card Component',
+    description:
+      'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
+    image: columnImage,
+    link: 'https://frontend-mentor-3-column-preview-component.netlify.app/',
+    github:
+      'https://github.com/lblake/Front-End-Projects/tree/main/3-column-preview-card-component',
+  },
+};
