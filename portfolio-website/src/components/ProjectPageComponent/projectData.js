109f30c100d28aa15a6678fb6cdc1be68521ad05
lblake <treleven.lloyd@gmail.com>
2022-02-07 11:31:51 +0000

    adjusted css for mobile first approach

--- portfolio-website/src/components/ProjectPageComponent/projectData.js
+++ portfolio-website/src/components/ProjectPageComponent/projectData.js
@@ -5,7 +5,7 @@ import filterImage from './images/Filter.jpg';
 
 export const projects = {
   pricing: {
-    title: 'CSS, HTML, JavaScript Component with Toggle',
+    title: 'CSS HTML JavaScript Component with Toggle',
     description:
       'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
     image: pricingImage,
@@ -13,6 +13,7 @@ export const projects = {
     github:
       'https://github.com/lblake/Front-End-Projects/tree/main/pricing-component-with-toggle',
   },
+
   weather: {
     title: 'React Weather API',
     description:
@@ -20,18 +21,19 @@ export const projects = {
     image: weatherImage,
     link: 'https://api-react-weather.netlify.app/',
     github: 'https://github.com/lblake/react-weather-api',
-
-    users: {
-      title: 'React API Users App',
-      description:
-        'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
-      image: filterImage,
-      link: 'https://react-users-api.netlify.app/',
-      github: 'https://github.com/lblake/react-api-users',
-    },
   },
+
+  users: {
+    title: 'React API Users App',
+    description:
+      'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
+    image: filterImage,
+    link: 'https://react-users-api.netlify.app/',
+    github: 'https://github.com/lblake/react-api-users',
+  },
+
   column: {
-    title: '3-Column Preview Card Component',
+    title: 'CSS HTML 3-Column Preview Card Component',
     description:
       'Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei',
     image: columnImage,
