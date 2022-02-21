3350abd45d0c55119768bcbbc61874ba56d5861b
lblake <treleven.lloyd@gmail.com>
2022-02-21 22:07:01 +0000

    removed unwanted code

--- portfolio-website/src/components/NavBarComponent/NavBar.js
+++ portfolio-website/src/components/NavBarComponent/NavBar.js
@@ -5,20 +5,7 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      {/* <a
-        className={styles.button}
-        href='/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <h1 className={styles.heading}>Portfolio</h1>
-      </a> */}
-      <Link
-        // className={styles.heading}
-        to='/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
+      <Link to='/' rel='noopener noreferrer'>
         <h1 className={styles.heading}>Portfolio</h1>
       </Link>
     </div>
