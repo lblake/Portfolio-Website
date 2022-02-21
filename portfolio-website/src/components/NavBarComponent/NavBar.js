717d38b2a10741342bfb63ea9b89cfe14114b2f8
lblake <treleven.lloyd@gmail.com>
2022-02-21 12:02:21 +0000

    added link to homepage

--- portfolio-website/src/components/NavBarComponent/NavBar.js
+++ portfolio-website/src/components/NavBarComponent/NavBar.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import styles from './navbar.module.css'
+import React from 'react';
+import styles from './navbar.module.css';
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div>
+      <a
+        className={styles.button}
+        href='/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <h1 className={styles.heading}>Portfolio</h1>
+      </a>
+    </div>
+  );
 }
