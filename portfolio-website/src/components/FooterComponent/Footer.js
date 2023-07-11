0e801ec23aa38a44ba7319d080324152a7136c6e
lblake <treleven.lloyd@gmail.com>
2023-07-11 13:21:48 +0100

    updated footer

--- portfolio-website/src/components/FooterComponent/Footer.js
+++ portfolio-website/src/components/FooterComponent/Footer.js
@@ -4,7 +4,7 @@ import styles from './footer.module.css';
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p>@2023 : lloydblake.dev</p>
+      <p>@ 2023 lloydblake.dev POWERED BY REACT </p>
     </div>
   );
 }
