882eb67159813fcdb1a7a13658e17f8a9a173a96
lblake <treleven.lloyd@gmail.com>
2023-07-23 15:55:52 +0100

    updated image on email success page

--- portfolio-website/src/components/ContactFormComponent/success.js
+++ portfolio-website/src/components/ContactFormComponent/success.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import styles from './success.module.css';
 import Footer from '../FooterComponent/Footer';
-import image from './zay-confirmation.png';
+import image from './success-image.png';
 
 function success() {
   return (
     <div className={styles.container}>
-      {/* <h2>Thank you for your message</h2> */}
+      <p className={styles.heading}>Your Message Has Been Sent</p> 
 
       <img className={styles.successImage} src={image} alt='' />
       <p className={styles.text}>
         Thank you for contacting me! I appreciate your email and the potential
         opportunity of working with you on your project(s). I will review your message
-        and contact you soon to provide the information you need or to answer
-        any questions you have asked me...
+        and contact you soon. I look forward to speaking to & working with you soon.
       </p>
       <Footer />
     </div>
