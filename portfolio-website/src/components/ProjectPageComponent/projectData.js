7863f506febdbbb252f145383dc6b3bcaa87aee5
lblake <treleven.lloyd@gmail.com>
2023-08-16 19:36:52 +0100

    updated text

--- portfolio-website/src/components/ProjectPageComponent/projectData.js
+++ portfolio-website/src/components/ProjectPageComponent/projectData.js
@@ -67,7 +67,7 @@ export const projects = {
   galaxy: {
     title: 'Galaxy Box Stars Single Product Shopify Store',
     description:
-      'This store was created using the <a href="https://tinyurl.com/dawn-shopify" target="_blank">Dawn </a> Shopify theme, this theme has a clean & minimalist design. The store was built to include the following features: a Wholesale page for customers to enquire about B2B opportunties, affiliate product page to allow the client to add affiliate items as required, an enhanced contact page with a "order number" field.  This theme also has other built-in features such as "Mega menu" & "Sticky Headers". ',
+      'This store was created using the <a href="https://tinyurl.com/dawn-shopify" target="_blank">Dawn </a> Shopify theme, this theme has a clean & minimalist design. The store was built to include the following features: a Wholesale page for customers to enquire about B2B opportunities, affiliate product page to allow the client to add affiliate items as required, an enhanced contact page with a "order number" field.  This theme also has other built-in features such as "Mega menu" & "Sticky Headers". ',
     image: galaxyImage,
     link: 'https://lloydblake.dev/galaxy',
     github: 'https://github.com/lblake/FreeMote-Projects/tree/main/galaxy-box',
