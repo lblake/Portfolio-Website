6924d2d3e21eddaec9f6347c2665bc7e441b77ea
lblake <treleven.lloyd@gmail.com>
2023-10-23 00:39:23 +0100

    modified text

--- portfolio-website/src/components/AboutComponent/About.js
+++ portfolio-website/src/components/AboutComponent/About.js
@@ -85,7 +85,7 @@ export default function About() {
           Equipping me with a broad range of skills including working as part of
           team, React, Typescript, Javascript, CSS, & HTML. I bring a diverse
           skill set acquired through the bootcamp(s), self-learning & over 10+
-          years of IT experience working in various sectors. Finally through the {' '}
+          years of IT experience working in various sectors. Finally the {' '}
           <a
             href='https://lunatemplates.co/pages/shopify-course-for-designers'
             target='_blank'
@@ -93,8 +93,8 @@ export default function About() {
           >
             Design Freedom on Shopify {' '}
           </a>
-          course. I have sharpened my design skills using tools such as Figma &
-          Adobe XD. To enable me to create any design on any Shopify theme. I am
+          course has been a game-changer for me. With newfound proficiency in Figma &
+          Adobe XD, I'm well-equipped to breathe life into any design on any Shopify theme. I am
           actively seeking new projects to bring captivating Shopify stores to
           life. Let's collaborate and create an impactful Shopify presence!.
         </p>
