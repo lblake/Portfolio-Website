d83fb66f4cd25e022b1f41be23617a7a4b6ad42e
lblake <treleven.lloyd@gmail.com>
2023-12-13 21:03:07 +0000

    updated profile photo

--- portfolio-website/src/components/AboutComponent/About.js
+++ portfolio-website/src/components/AboutComponent/About.js
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 
 export default function About() {
   const image = new Image();
-  image.src = require('./my-image.avif');
+  image.src = require('./smile-photo.jpeg');
 
   return (
     <div className={styles.aboutContainer}>
